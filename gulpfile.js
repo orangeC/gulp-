09786d68efafff6e9670951d211ca8a250773247
orangeC <18332563755@163.com>
2016-11-03 16:20:06 +0800

    sass功能：变量使用

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,10 @@ gulp.task("sass",function(){
 	.pipe(sass())
 	.pipe(gulp.dest("dist/"));
 })
+
+gulp.task("copy-assets",function(){//拷贝Html文件到dist中
+	gulp.src("src/*.html")//锁定要拷贝的所有html文件
+	.pipe(gulp.dest("dist/"));
+})
+
+gulp.task('default', ['sass', 'copy-assets']);//default同时对应这两个
