c7cbd8f584085f9852f493730e638c8656eca2d4
orangeC <18332563755@163.com>
2016-11-03 20:40:07 +0800

    gulp-minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var prefix = require("gulp-autoprefixer");//引入autoprefixer
+var minify = require("gulp-minify-css")
 
 
 gulp.task("sass",function(){
 	gulp.src("src/main.scss")
 	.pipe(sass())
 	.pipe(prefix())//在sass转化css之后进行厂商前缀的添加
+	.pipe(minify())//压缩css文件
 	.pipe(gulp.dest("dist/"));
 })
 
